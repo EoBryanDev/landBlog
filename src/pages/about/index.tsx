d624a92f741f4eb339594ed4ecc46f9f16d844ba
EoBryanDev <mauriciobryan1998@gmail.com>
2023-10-09 17:00:14 -0300

    Creating About Us Page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from "next";
+
+import Head from "next/head";
+
+import styles from "./styles.module.scss";
+import { getPrismicClient } from "@/src/services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+import { FaYoutube, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+
+type Content = {
+  title: string;
+  description: string;
+  banner: string;
+  facebook: string;
+  instagram: string;
+  youtube: string;
+  linkedin: string;
+};
+
+interface ContentProps {
+  content: Content;
+}
+
+const About = ({ content }: ContentProps) => {
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>{content.title}</h1>
+            <p>{content.description}</p>
+
+            <a href={content.youtube}>
+              <FaYoutube size={40} />
+            </a>
+            <a href={content.instagram}>
+              <FaInstagram size={40} />
+            </a>
+            <a href={content.facebook}>
+              <FaFacebook size={40} />
+            </a>
+            <a href={content.linkedin}>
+              <FaLinkedin size={40} />
+            </a>
+          </section>
+
+          <img src={content.banner} alt='About Us' />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const resp = await prismic.query([
+    Prismic.Predicates.at("document.type", "about"),
+  ]);
+
+  const { title, description, banner, facebook, instagram, youtube, linkedin } =
+    resp.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    description: RichText.asText(description),
+    banner: banner.url,
+    facebook: facebook.url,
+    instagram: facebook.url,
+    youtube: youtube.url,
+    linkedin: linkedin.url,
+  };
+  return {
+    props: {
+      content,
+    },
+    revalidate: 60*60*2
+  };
+};
